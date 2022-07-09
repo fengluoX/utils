9efad027a25a34bd45ad8abd4b0303ccb0513d57
luoye <1535985372@qq.com>
2022-07-09 17:42:54 +0800

    feat: 增加eslint和prettierrc配置

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,12 +1,10 @@
 export const sym = Symbol("123123123");
 
-const consoleHelloWorld = () => {
-  return new Promise((resolve) => {
+const consoleHelloWorld = () =>
+  new Promise((resolve) => {
     setTimeout(() => {
-      console.log("hello World", sym);
       resolve(123);
     }, 1000);
   });
-};
 
 export default consoleHelloWorld;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import helloWorld from "./helloWorld";
+
 type MyPick<T, K extends keyof T = keyof T> = {
   [P in K]: T[P];
 };
-let a: MyPick<{ a: string }>;
 
-a = {
+const a: MyPick<{ a: string }> = {
   a: "1231",
 };
 
-export const consoleHelloWorld = () => {
-  return new Promise((resolve) => {
+export const consoleHelloWorld = () =>
+  new Promise((resolve) => {
     setTimeout(() => {
-      console.log("hello World");
+      console.log(2123213);
+
+      console.log(12321321);
       resolve(123);
     }, 1000);
   });
-};
 
-helloWorld()
+helloWorld();
 
 export default a;
